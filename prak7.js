9353182e06f7ec2f8091665a4bcfd091a5119d5d
anggia intan <anggiaaantn@gmail.com>
2024-04-06 11:47:07 +0700

    first commit

---
+++ prak7.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video One Thing by One Direction");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio One Thing by One Direction");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Foto One Direction");
+});
